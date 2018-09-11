0cfb22492dec0ec0ff49b6b2c42ceabaceb4130a
doyongju <doyungju@naver.com>
2018-09-11 12:51:10 +0900

    Revert "version3 text added"
    This reverts commit d588386d04c1618f4e859f504350df699b4c277c.

--- java.java
+++ java.java
@@ -2,6 +2,5 @@ Public class Main{
    public static void main(string[] args){
       System.out.println("Hello Web&App");
       System.out.println("Hello version2"); 
-      System.out.println("Hello version3"); 
     }
 }
